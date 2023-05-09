7bc1840c5bbc3a1853da30a739af3cee9d8e5d5b
nicdo77 <nicolas.daudin@gmail.com>
2023-05-09 16:36:41 +0200

    Revert "feat(view): add field to search for ASIN"
    This reverts commit afc31041acd5ba6ee1af61a6e44cba7dc0eafa68.

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get, Param, Query, Render } from '@nestjs/common';
+import { Controller, Get, Param, Render } from '@nestjs/common';
 import { BooksService } from './books.service';
 
 @Controller('book')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
-  @Get() // 2205088165 or B09ZYQRVQB
+  @Get(':asin') // 2205088165 or B09ZYQRVQB
   @Render('books')
-  async findByAsin(@Query('asin') asin: string) {
+  async findByAsin(@Param('asin') asin: string) {
     const book = await this.booksService.findByAsin(asin);
     return { book };
   }
