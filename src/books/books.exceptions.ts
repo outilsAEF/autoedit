cca2e355a74c0f1f40b95ae89a5e928410ce7bb3
nicdo77 <nicolas.daudin@gmail.com>
2023-05-11 16:54:26 +0200

    feat(books): nice interface with Bootstrap. Closes #13.

--- src/books/books.exceptions.ts
+++ src/books/books.exceptions.ts
@@ -4,3 +4,6 @@ export class InvalidASINException extends Error {
     this.name = 'InvalidASINException';
   }
 }
+
+// HINT
+// The global exception filter partially supports the http-errors library. Basically, any thrown exception containing the statusCode and message properties will be properly populated and sent back as a response (instead of the default InternalServerErrorException for unrecognized exceptions).
