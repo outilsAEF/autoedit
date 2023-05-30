28d0f978f0b9bacebca1ec0754abbb2c63eab2e2
nicdo77 <nicolas.daudin@gmail.com>
2023-05-30 16:51:43 +0200

    fix(view): remove logs and added status 404 for non existent ressource (cart.json, ...)

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -1,4 +1,4 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
 import { Response } from 'express';
 
 @Catch(HttpException)
@@ -6,8 +6,11 @@ export class HttpExceptionFilter<T extends HttpException> implements ExceptionFi
   catch(exception: T, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const res = ctx.getResponse<Response>();
-    console.error(exception);
 
+
+    if (exception.getStatus() === HttpStatus.NOT_FOUND) return res.status(HttpStatus.NOT_FOUND).json({ statusCode: HttpStatus.NOT_FOUND });
+
+    console.error(exception);
     res.render('index', {
       title: `Erreur interne`,
       error: {
